34870cc1faadddc45c2e75fca4a2d5a2c82c5be4
Veysel <veyselanik@sabanciuniv.edu>
2022-09-06 16:24:10 +0300

    ImageView controllers added

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -1,7 +1,7 @@
 //
 //  ViewController.swift
 //  Dicee-iOS13
-// va
+//
 //  Created by Angela Yu on 11/06/2019.
 //  Copyright © 2019 London App Brewery. All rights reserved.
 //
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+
     }
 
-
+    
+    
 }
 
